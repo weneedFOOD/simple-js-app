9934684c948d1a1921bf6d4428521f2ac6c3f6d1
weneedFOOD <weneedfood.jla@gmail.com>
2022-10-15 01:53:32 -0500

    Update scripts.js
    feat!: added new variable 'pokemonlist'
    
    Created three Pokemon objects in 'pokemonlist' array.

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+let pokemonlist = [];
+  { name: 'Balbasure', height 7, type: ['grass', 'poison']},
+  { name: 'Blastoise', height 1.6, type: ['water']},
+  { name: 'Mew', height 0.4, type: ['psychic', 'flight']}
