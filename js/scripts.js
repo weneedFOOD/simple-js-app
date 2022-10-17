69496f45179dec9ae2edfe34e1a953d0eb7dd6ce
weneedFOOD <weneedfood.jla@gmail.com>
2022-10-16 20:10:32 -0500

    Update scripts.js
    fix!: declared objects within the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-let pokemonlist = [];
-  { name: 'Balbasure', height 7, type: ['grass', 'poison']},
-  { name: 'Blastoise', height 1.6, type: ['water']},
-  { name: 'Mew', height 0.4, type: ['psychic', 'flight']}
+let pokemonList=[{name: "Balbasure", height: 7, type: ["grass","poison"]},{name: "Charizard", height: 1.7, type:["fire","flying"]},{name: "Mew", height: 0.4, type: "psychic"}];
+
+console.log(pokemonList);
