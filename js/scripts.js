bd17a87bca9c0d561ac417a720107d017c516959
weneedFOOD <weneedfood.jla@gmail.com>
2022-10-23 17:53:44 -0500

    Update scripts.js
    feat!: added a for loop to iterate over each object within the array
    
    Also added conditional statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,42 @@
-let pokemonList=[{name: "Balbasure", height: 7, type: ["grass","poison"]},{name: "Charizard", height: 1.7, type:["fire","flying"]},{name: "Mew", height: 0.4, type: "psychic"}];
+let pokemonList=[
+  {
+    name: "Balbasure", height: 0.7, type: ["grass","poison"]
+  },
+  {
+    name: "Charizard", height: 1.7, type:["fire","flying"]
+  },
+  {
+    name: "Mew", height: 0.4, type: ["psychic"]
+  }
+];
 
-console.log(pokemonList);
+for (let i = 0;
+    i < pokemonList.length; i++) {
+    if(pokemonList[i].height >= 5) {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - Wow, that is big!" + "<br>")
+    }
+    else if (pokemonList[i].height >= 2 && pokemonList[i].height < 5){
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That is a medium pokemon!" + "<br>")
+    }
+    else {
+        document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + " m) - That is a small pokemon!" + "<br>")
+    }
+}*/
+
+/*let pokemonRepository = [
+    {
+      name: "Balbasure",
+      height: 0.7,
+      types: ["grass", "poison"]
+    },
+    {
+      name: "Charizard",
+      height: "1.7",
+      types: ["fire", "flying"]
+    },
+    {
+      name: "Mew",
+      height: 0.4,
+      types: ["psychic"]
+    }
+  ];          
