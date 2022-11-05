8c1f08ec73d8e07fc8af3a5f601bf43e007d9d56
weneedFOOD <weneedfood.jla@gmail.com>
2022-11-04 22:39:00 -0500

    Update scripts.js
    feat!: wrapped pokemonList array in a IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,46 @@ let pokemonRepository = [
       types: ["psychic"]
     }
   ];
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  
+  function add(pokemon) {
+    if (typeof pokemon === 'object' && 'name' in pokemon){
+    pokemonList.push(pokemon);  
+    }
+  }
+
+   function addListItem(pokemon) {
+     let pokemonList = document.querySelector('.pokemon-list');
+
+     let listItem = document.createElement('li');
+
+     let button = document.createElement('button');
+     button.innerText = pokemon.name
+     button.classList.add('button-class');
+
+     listItem.appendChild(button);
+     pokemonList.appendChild(listItem);
+
+     button.addEventListener('click', function(event) {
+                showDetails(pokemon)
+        })
+     }
+
+     function showDetails(pokemon) {
+       console.log(pokemon);
+     }
+
+
+      return {
+        add: add,
+        getAll: getAll,
+        addListItem: addListItem,
+        showDetails: showDetails
+      }
+
+
+   
